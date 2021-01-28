88e823782503a5bc169fd50c2dda1929b60b51ad
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2021-01-28 10:37:05 -0500

    Weird fixes for Rcpp
    For some reason, I now have to `import(Rcpp)` in my NAMESPACE.
    Otherwise, I get errors about `function enterRNGscope not provided by
    Rcpp`. This workaround seems to make the errors go away.

--- src/solvearray.cpp
+++ src/solvearray.cpp
@@ -2,12 +2,17 @@
 
 // [[Rcpp::depends(RcppArmadillo)]]
 
+//' Solve the matrix equation AX = B for X for an array of matrices A
+//'
+//' @param a 3D array. For dimensions x,y,z, iteration is over dimension z ("slices")
+//' @param b Matrix with dimensions y,x
+//' @return Matrix of solutions (x,z)
+//' @import Rcpp
 // [[Rcpp::export]]
 arma::mat solvearray(const arma::cube& a, const arma::mat& b) {
   unsigned int nwl = a.n_slices, s = a.n_cols;
   unsigned int w;
   arma::mat result(s, nwl);
-  // arma::sp_mat M;
 
   for (w = 0; w < nwl; w++) {
     result.col(w) = arma::solve(a.slice(w), b.col(w), arma::solve_opts::fast);
