bcd0ca703cb41c157ab686124d335008571db4e3
Alexey Shiklomanov <alexey.shiklomanov@nasa.gov>
2026-03-30 14:55:24 -0400

    update sensor RSR object names

--- R/spectral-response.R
+++ R/spectral-response.R
@@ -1,32 +1,31 @@
 #' Sensor spectral response functions
-#' 
+#'
 #' @export
-sensor.list <- c("identity", "aviris.ng", "aviris.classic",
+sensor_list <- c("identity", "aviris.ng", "aviris.classic",
                  "hyperion", "chris.proba", "landsat5", "landsat7",
                  "landsat8", "modis", "viirs", "avhrr", "licor")
 
 #' Sensor list with proper names
-#' 
+#'
 #' @export
-sensor.proper <- c("ASD Field Spec", "AVIRIS NG", "AVIRIS Classic",
+sensor_proper <- c("Native", "AVIRIS NG", "AVIRIS Classic",
                    "Hyperion", "CHRIS-Proba", "Landsat 5", "Landsat 7",
                    "Landsat 8", "MODIS", "VIIRS", "AVHRR", "LiCor 6400 chamber")
 
-names(sensor.proper) <- sensor.list
+names(sensor_proper) <- sensor_list
 
 #' Convolution of spectra to sensor RSR
-#' 
+#'
 #' @param spec Full (1 nm) spectrum (vector)
 #' @param sensor Sensor name (string). See sensor.list
 #' @export
-spectral.response <- function(spec, sensor) {
+spectral_response <- function(spec, sensor) {
   sensor <- tolower(sensor)
-  stopifnot(sensor %in% sensor.list)
+  stopifnot(sensor %in% sensor_list)
   if (sensor == "identity") {
     return(spec)
   }
-  rsr <- sensor.rsr[[sensor]]
+  rsr <- sensor_rsr[[sensor]]
   m <- spec[rsr[, "index"]] * rsr[, -1]
-  out.spec <- .colSums(m, nrow(m), ncol(m))
-  return(out.spec)
+  .colSums(m, nrow(m), ncol(m))
 } # spectral.response
